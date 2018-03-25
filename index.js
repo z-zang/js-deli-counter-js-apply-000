99fbb6c5f6cf430a3e411e55b78f5c4cd5ba9675
Zichao Zang <zang.code@gmail.com>
2018-03-25 22:04:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(line, name){
 }
 
 function nowServing(){
-  if (!line.length){
+  if (line.length == 0){
     return "There is nobody waiting to be served!"
   } else {
   return "Currently serving " + line.shift();
