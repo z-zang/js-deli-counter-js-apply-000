19bb536096bd81f89dd51f2f110cfcaa65662fd3
Zichao Zang <zang.code@gmail.com>
2018-03-25 21:52:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var line = [];
 function takeANumber(line, name){
   line.push(name);
   
-  return ("Welcome, " + name + ". You are number " + line.length + " in line"); 
+  return ("Welcome, " + name + ". You are number " + line.length + " in line."); 
 
 }
 
