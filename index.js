73b1ec718697ae8cdadf695a9b3cbdca2011036c
Zichao Zang <zang.code@gmail.com>
2018-03-25 22:30:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ function currentLine(line){
   if (!line.length) {
     return "The line is currently empty.";
   } 
+  
+  var 
   for (var i=0; i<line.length; i++) {
     lineNamesandNumbers.push(i+1 + ". "+ line[i]);
   }
-  console.log("The line is currently: " + lineNamesandNumbers)
   return "The line is currently: " + lineNamesandNumbers.join(', ');
 }
 
