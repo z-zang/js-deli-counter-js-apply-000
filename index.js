e000ddfb856e9fea6b2d06f6b08111a6af03d6f8
Zichao Zang <zang.code@gmail.com>
2018-03-25 21:42:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
+ console.log("") 
   
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
 }
