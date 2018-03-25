76816edf5b6ab9324a9e4c9c17db5fe28e3f786b
Zichao Zang <zang.code@gmail.com>
2018-03-25 21:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
- console.log("Welcome, " + name + ". You are number " +line.length +" in line") 
+ 
+  console.log("Welcome, " + name + ". You are number " +line.length +" in line"); 
   
   line.push(name);
   
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+
 }
-
-
-"Welcome, Ada. You are number 1 in line.")
